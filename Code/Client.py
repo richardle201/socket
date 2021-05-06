0d967f896cf6d2fa558aba02daa12e92b91da9fc
ngoclamck14 <66857056+ngoclamck14@users.noreply.github.com>
2021-05-06 16:28:49 +0700

    Final

--- Code/Client.py
+++ Code/Client.py
@@ -1,7 +1,6 @@
 import socket
 
 
-
 # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
@@ -9,22 +8,27 @@ port = 1233
 # connect the client
 # client.connect((target, port))
 client.connect((host, port))
-response = client.recv(2048)
-# Input UserName
-name = input(response.decode())	
-client.send(str.encode(name))
-response = client.recv(2048)
-# Input Password
-password = input(response.decode())	
-client.send(str.encode(password))
-''' Response : Status of Connection :
-	1 : Registeration successful 
-	2 : Connection Successful
-	3 : Login Failed
-'''
-# Receive response 
-response = client.recv(2048)
-response = response.decode()
 
-print(response)
-client.close()
+while True:
+
+	response = client.recv(2048)
+	# Input UserName
+	name = input(response.decode())	
+	client.send(str.encode(name))
+	response = client.recv(2048)
+	# Input Password
+	password = input(response.decode())	
+	client.send(str.encode(password))
+
+	''' Response : Status of Connection :
+		1 : Registeration successful 
+		2 : Username already existed
+	'''
+	# Receive response 
+	response = client.recv(2048)
+	response = response.decode()
+
+	print(response)
+	if response == 'Registeration Successful':
+		client.close()
+		break
