09e92642943e193b851897de9aa4a1f72b2fe76d
ngoclamck14 <66857056+ngoclamck14@users.noreply.github.com>
2021-05-06 16:29:18 +0700

    Final

--- Code/Server.py
+++ Code/Server.py
@@ -20,34 +20,31 @@ HashTable = {}
 
 # Function : For each client 
 def threaded_client(connection):
-    connection.send(str.encode('ENTER USERNAME : ')) # Request Username
-    name = connection.recv(2048)
-    connection.send(str.encode('ENTER PASSWORD : ')) # Request Password
-    password = connection.recv(2048)
-    password = password.decode()
-    name = name.decode()
-    #Ma hoa password
-    #password=hashlib.sha256(str.encode(password)).hexdigest() # Password hash using SHA256
+    while True:
+        connection.send(str.encode('ENTER USERNAME : ')) # Request Username
+        name = connection.recv(2048)
+        connection.send(str.encode('ENTER PASSWORD : ')) # Request Password
+        password = connection.recv(2048)
+        password = password.decode()
+        name = name.decode()
+        #Ma hoa password
+        password=hashlib.sha256(str.encode(password)).hexdigest() # Password hash using SHA256
 # REGISTERATION PHASE   
 # If new user,  regiter in Hashtable Dictionary  
-    if name not in HashTable:
-        HashTable[name]=password
-        connection.send(str.encode('Registeration Successful')) 
-        print('Registered : ',name)
-        print("{:<8} {:<30}".format('USER','PASSWORD'))
-        for k, v in HashTable.items():
-            label, num = k,v
-            print("{:<8} {:<30}".format(label, num))
-        print("-------------------------------------------")
-        
-    else:
-# If already existing user, check if the entered password is correct
-        if(HashTable[name] == password):
-            connection.send(str.encode('Connection Successful')) # Response Code for Connected Client 
-            print('Connected : ',name)
+        if name not in HashTable:
+            HashTable[name]=password
+            connection.send(str.encode('Registeration Successful')) 
+            print('Registered : ',name)
+            print("{:<8} {:<20}".format('USER','PASSWORD'))
+            for k, v in HashTable.items():
+                label, num = k,v
+                print("{:<8} {:<20}".format(label, num))
+            print("-------------------------------------------")
+            break
         else:
-            connection.send(str.encode('Login Failed')) # Response code for login failed
-            print('Connection denied : ',name)
+# If already existing user, check if the entered password is correct
+            connection.send(str.encode('Username already existed'))
+            print('Sign up failed')
     while True:
         break
     connection.close()
