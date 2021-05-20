1bafb46cc36d296443d2b5534cb0579e6e955552
richardle201 <60380948+richardle201@users.noreply.github.com>
2021-05-20 23:44:35 +0700

    Update sv.py

--- sv.py
+++ sv.py
@@ -1,52 +1,12 @@
 import socket
-import takepic
-import keylog
-import shutdown
 
-
-def receiveSignal():
-    choice = 'QUIT'
-    try:
-        choice = (conn.recv(2048)).decode()
-    except:
-        choice = 'QUIT'
-    finally:
-        return choice
-    
-def TAKEPIC():
-    screenshot_data = takepic.start_takepic()
-    conn.sendall(screenshot_data)
-
-
-def listen_choices():
-    while True:
-        choice = receiveSignal()
-        if choice == 'KEYLOG':
-            #keylog.start_keylog()
-            break
-        elif choice == 'SHUTDOWN':
-            #shutdown.start_shutdown()
-            break
-        # # elif choice =='REGISTRY':
-        # #     registry()
-        # #     break
-        elif choice == 'TAKEPIC':
-            takepic.start_takepic()
-            break
-        # # elif choice == 'PROCESS':
-        # #     process()
-        # #     break
-        # # elif choice == 'APPLICATION':
-        # #     application()
-        # #     break
-        elif choice == 'QUIT': break
-  
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
 print ("Socket successfully created")
 ip = socket.gethostname()
+print(ip)
 
-port = 12345                
+port = 2345                
   
 s.bind((ip, port))         
 print ("socket binded to %s" %(port)) 
@@ -59,9 +19,7 @@ while True:
 # Establish connection with client. 
     conn, addr = s.accept()     
     print ('Got connection from', addr )
-    
-    listen_choices()
+    conn.send(b'Connecting...')
     conn.close()
-    break
 # Close the connection with the client 
 s.close()
