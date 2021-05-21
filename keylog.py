e55cb2f2239e46d06d49bb3f63725ad35fb28d89
ngoclamck14 <66857056+ngoclamck14@users.noreply.github.com>
2021-05-21 23:31:43 +0700

    Update keylog.py

--- keylog.py
+++ keylog.py
@@ -22,3 +22,50 @@ def start_keylog():
         res = file.read()
     os.remove('log.txt')
     return res
+
+
+#Cliet-GUI
+import tkinter as tkr
+from tkinter import *
+import tkinter.ttk as exTK
+from tkinter import scrolledtext as scrllT
+
+win = tkr.Tk()
+win.title('listApp')
+win.geometry('500x500')
+
+def hook():
+    #Gửi báo hiệu keylog
+    a='' #Để cho đỡ báo lỗi
+
+def unhook():
+    #Gửi báo hiệu thoát keylog
+    a=''
+
+def xoa():
+    global output
+    output.configure(state=NORMAL)
+    output.delete('1.0', END)
+
+
+def inphim():
+    global output
+    data=''   #Dữ liệu nhận từ server 
+    output.configure(state=NORMAL)
+    output.insert(END,data)
+    output.configure(state=DISABLED)
+
+hook_ = exTK.Button(win, text='Hook').place(
+    relheight=0.1, relwidth=0.2, relx=0.04, rely=0.075)
+unhook_ = exTK.Button(win, text='Unhook').place(
+    relheight=0.1, relwidth=0.2, relx=0.28, rely=0.075)
+inphim_ = exTK.Button(win, text='In phím',command=inphim).place(
+    relheight=0.1, relwidth=0.2, relx=0.52, rely=0.075)
+xoa_ = exTK.Button(win, text='Xoá',command=xoa).place(
+    relheight=0.1, relwidth=0.2, relx=0.76, rely=0.075)
+
+global output
+output = scrllT.ScrolledText(win, font='Calibri 12',state=DISABLED)
+output.place(relheight=0.6, relwidth=0.88, relx=0.04, rely=0.25)
+
+win.mainloop()
