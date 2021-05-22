316f0d8f20d54fc770fe7afbe5154deb97c8d965
richardle201 <60380948+richardle201@users.noreply.github.com>
2021-05-22 20:09:41 +0700

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,42 +1,52 @@
 import socket
+class SocketError(Exception):
+    pass
+class Socket:
+    def __init__(self,host=socket.gethostname(),port=2345,verbose=0):
+        self.host=host
+        self.port=port
+        self.SocketError=SocketError()
+        self.verbose=verbose
+        try:
+            if self.verbose:
+                print('SocketUtils:Creating Socket()')
+            self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error:
+            raise SocketError('Error in Socket Object Creation!!')
+    def Close(self):
+        if self.verbose:
+            print('SocketUtils:Closing socket!!')
+        self.sock.close()
+        if self.verbose:
+            print('SocketUtils:Socket Closed!!')
+    def __str__(self):
+        return 'SocketUtils.Socket\nSocket created on Host='+str(self.host)+',Port='+str(self.port)
+class SocketClient(Socket):
+    status = False
+    def Connect(self,rhost=socket.gethostname(),rport=2345):
+        self.rhost,self.rport=rhost,rport
+        try:
+            if self.verbose:
+                print('Connecting to '+str(self.rhost)+' on port '+str(self.rport))
+            self.sock.connect((self.rhost,self.rport))
+            if self.verbose:
+                self.status = True
+                print('Connected !!!')
+        except socket.error:
+            raise SocketError('Connection refused to '+str(self.rhost)+' on port '+str(self.rport))
+    def Send(self,data):
+        if self.verbose:
+            print('Sending data of size ',len(data))
+        if type(data) == str:
+            data = data.encode()
+        self.sock.send(data)
+        if self.verbose:
+            print('Data sent!!')
+    def Receive(self,size=1024):
+        if self.verbose:
+            print('Receiving data...')
+        return self.sock.recv(size).decode()
+    def __str__(self):
+        return 'SocketUtils.SocketClient\nClient connected to Host='+str(self.rhost)+',Port='+str(self.rport)
 
-def TAKEPIC():
-    screenshot_data = s.recv(2048)
-    # print(screenshot_data)
-    with open('new_screenshot.png','wb') as f:
-        f.write(screenshot_data)
-# Create a socket object 
-# s = socket.socket()         
-ip = socket.gethostname()
-# Define the port on which you want to connect 
-port = 12345    
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip, port)) 
-while True:
-    choice = input('Enter choice: ')
-    s.send(choice.encode())
-    # if s == 'TAKEPIC':
-    #     takepic()
-    # # receive data from the server 
-    # # close the connection 
-    # elif s == 'QUIT': break
-    if choice == 'KEYLOG':
-        #keylog.start_keylog()
-        break
-    elif choice == 'SHUTDOWN':
-        break
-    # # elif choice =='REGISTRY':
-    # #     registry()
-    # #     break
-    elif choice == 'TAKEPIC':
-        TAKEPIC()
-        break
-    # # elif choice == 'PROCESS':
-    # #     process()
-    # #     break
-    # # elif choice == 'APPLICATION':
-    # #     application()
-    # #     break
-    elif choice == 'QUIT': break
-s.close()
 
