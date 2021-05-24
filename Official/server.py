d6bf893d9ca4d08e75cae54a13a9f6a7ce2fa194
ngoclamck14 <66857056+ngoclamck14@users.noreply.github.com>
2021-05-25 01:33:58 +0700

    Update server.py

--- Official/server.py
+++ Official/server.py
@@ -1,29 +1,64 @@
-from tkinter import *
-import tkinter.ttk as exTK
 import socket
 import os
 import process
 import keylog
 import screen
+import registry
 from struct import *
 import pickle
-import socket_class as SC
-class SocketServer(SC.Socket):
-    def __init__(self,host=socket.gethostname(),port=2345):
-        self.host,self.rhost=host,host
-        self.port,self.rport=port,port
+from tkinter import *
+import tkinter.ttk as exTK
+
+
+class SocketError(Exception):
+    pass
+class Socket:
+    def __init__(self,host=socket.gethostname(),port=2345,verbose=0):
+        self.host=host
+        self.port=port
+        self.SocketError=SocketError()
+        self.verbose=verbose
         try:
+            if self.verbose:
+                print('SocketUtils:Creating Socket()')
             self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         except socket.error:
-            raise SC.SocketError('Failed to create SocketServer object!!')
+            raise SocketError('Error in Socket Object Creation!!')
+    def Close(self):
+        if self.verbose:
+            print('SocketUtils:Closing socket!!')
+        self.sock.close()
+        if self.verbose:
+            print('SocketUtils:Socket Closed!!')
+class SocketServer(Socket):
+    def __init__(self,host=socket.gethostname(),port=2345,verbose=0):
+        self.host,self.rhost=host,host
+        self.port,self.rport=port,port
+        self.verbose=verbose
         try:
+            if self.verbose:
+                print('SocketUtils:Creating SocketServer()')
+            self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        except socket.error:
+            raise SocketError('Failed to create SocketServer object!!')
+        try:
+            if self.verbose:
+                print('SocketUtils:Binding Socket()')
             self.sock.bind((self.host,self.port))
+            if self.verbose:
+                print (self)
         except socket.error as msg:
-            raise SC.SocketError(msg)
+            raise SocketError(msg)
     def Listen(self,msg='Accepted Connection from:'):
+        if self.verbose:
+            print('Listening to port')%self.port
         self.sock.listen(1)
         self.conn,self.rhost=self.sock.accept()
         self.rhost=self.rhost[0]
+        if self.rhost:
+            if self.verbose:
+                print('Got connection from',self.rhost)
+        #print (msg,self.rhost)
     def Send(self, obj):
         msg = pickle.dumps(obj)
         length = pack('>Q',len(msg))
@@ -46,6 +81,8 @@ class SocketServer(SC.Socket):
             img = screen.screenshot()
             self.Send(img)
         elif msg == 'Shutdown':
+            self.conn.close()
+            self.Close()
             os.system("shutdown /s /t 30")
         elif msg == 'List process':
             listprocess = process.getListProcess()
@@ -92,14 +129,29 @@ class SocketServer(SC.Socket):
             keylog.unhook()
         elif msg == 'Print key':
             self.Send(keylog.get_key())
+        elif msg == 'import':
+            data = self.Receive()
+            registry.import_filereg(data)
+        elif msg[0] == 'Get value':
+            data = registry.getValue(msg[1],msg[2])
+            self.Send(data)
+        elif msg[0] == 'Set value':
+            self.Send('Set')
+        elif msg[0] == 'Delete value':
+            self.Send('Delete')
+        elif msg[0] == 'Create key':
+            self.Send('Create')
+        elif msg[0] == 'Delete key':
+            self.Send('Delete')
         elif msg == 'Quit':
             self.conn.close()
             self.Close()
             exit()
 
-
 def startServer():
     sv = SocketServer()
+    print(socket.gethostname())
+
     while True:
         sv.Listen()
         while True:
@@ -107,8 +159,9 @@ def startServer():
             sv.Choices(msg)
 
 win = Tk()
-win.geometry('275x275')
+win.geometry('275x275+500+300')
 win.title('Server')
 exTK.Button(win,text='Mở server',command=startServer).place(relheight=0.6,
             relwidth=0.6,relx=0.2,rely=0.2)
 win.mainloop()
+
